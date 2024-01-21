c9738ae2e35f35d8f95e8da944a48a6c1bcaefb1
Mariane <mariane.ferreiraanjos@outlook.com>
2024-01-21 18:39:08 -0300

    atualizações no keyboard

--- jogo-da-forca/src/App.tsx
+++ jogo-da-forca/src/App.tsx
@@ -8,23 +8,30 @@ import Keyboard from './components/keyboard'
 const Wrapper = styled.div`
 display: flex;
 flex-direction: column;
+align-items: center;
 gap: 2rem;
 `
 
 
-
+const HanfmanPart = styled.div`
+display: flex;
+flex-direction: column;
+gap: 2rem;
+widith:375px;
+`
 
 function App() {
     return (
-      <Wrapper>
-        <h2> Jogo da forca - Mariane Anjos</h2>
-
-        {/* Construção dos componentes */}
-        <HangmanDrawing/>
-        <HangmanWord/>
-        <Keyboard/>
-      </Wrapper>
-
+      <>
+        <Wrapper/>
+          <HanfmanPart>
+            <h2> Jogo da forca - Mariane Anjos</h2>
+            {/* Construção dos componentes */}
+            <HangmanDrawing/>
+            <HangmanWord/>
+            </HanfmanPart>
+            <Keyboard/>
+      </>
   )
 }
 
--- jogo-da-forca/src/components/keyboard.tsx
+++ jogo-da-forca/src/components/keyboard.tsx
@@ -1,5 +1,72 @@
+import styled from "styled-components"
+
+const keys = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'ã',
+    'õ',
+    'ç',
+    'é',
+]
+
+const Wrapper = styled.div`
+display: grid;
+grid-template-columns: repeat(auto-fit,minmax(75px, 1fr));
+gap: 0.5rem;y
+width: 600px;
+`
+const Button = styled.button<{isActive: boolean}>`
+    opacity: ${(p) => p.isActive ? 'null': '0.3'};
+
+    &: focus:disabled {
+        outline: none;
+        border-color: transparent;
+        cursos: not-allowed;
+    }
+    &: focus-visible:disabled{
+        outline: none;
+        border-color: transparent;
+        cursor: not-allowed;
+    }
+    &: hover:disabled{
+        outline: none;
+        border-collor: transparent;
+        cursor: not-allowd;
+    }
+`
+/*Keys feiito com map */
 export default function Keyboard(){
     return(
-        <div>keyboard</div>
+        <Wrapper>
+            {keys.map((letter) => (
+            <Button isActive={true} key={letter}>
+                {letter.toUpperCase()}
+                </Button>
+            ))}
+        </Wrapper>
     )
 }
