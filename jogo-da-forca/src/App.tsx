84f83e8b4cb49c03fa898f51201c157629891438
Mariane <mariane.ferreiraanjos@outlook.com>
2024-01-21 15:49:15 -0300

    construção para o teste das palavras escondidas

--- jogo-da-forca/src/App.tsx
+++ jogo-da-forca/src/App.tsx
@@ -1,20 +1,30 @@
+import { styled } from 'styled-components'
 import './App.css'
-import HangmanDrawing from './components/hangman-Drawing'
+import HangmanDrawing from './components/hangman-drawing'
 import HangmanWord from './components/hangman-word'
 import Keyboard from './components/keyboard'
 
+
+const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+gap: 2rem;
+`
+
+
+
+
 function App() {
     return (
-    <>
-      <div className="app">
+      <Wrapper>
         <h2> Jogo da forca - Mariane Anjos</h2>
 
         {/* Construção dos componentes */}
         <HangmanDrawing/>
         <HangmanWord/>
         <Keyboard/>
-      </div>
-    </>
+      </Wrapper>
+
   )
 }
 
--- jogo-da-forca/src/components/hangman-word.tsx
+++ jogo-da-forca/src/components/hangman-word.tsx
@@ -1,5 +1,30 @@
-export default function HangmanWord(){
+import styled from "styled-components"
+
+
+/*Construção dos mostradores de letras*/
+
+
+const Wrapper = styled.div`
+display: flex;
+justify-content: center;
+gap: 0.5rem;
+font-size: 4rem;
+font-weigth: bold;
+text-transform: uppercase;
+font-family: sans-serif;
+`
+
+export default function HangmandWord(){
+
+    const word ='teste'
+
     return(
-        <div>hangman-word</div>
+        <Wrapper>
+            {word.split('').map((letter, index) => (
+                <span style={{ borderBottom: '0.1em solid black' }}  key={index}>
+                    {letter}
+                    </span>
+            ))} 
+        </Wrapper>
     )
 }
